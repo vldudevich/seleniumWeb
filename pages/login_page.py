6cde73cfce9718b18caa347d1110d49cf53ee83d
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-29 18:38:32 +0300

    test when switching between pages

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "what it this?" in self.browser.currect_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", "No login url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No autorizacion form"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER_FORM), "No register form"
+        assert True
