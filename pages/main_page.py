ae53b194a3a6924d147c99e39d01187cbfe7bbb1
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-29 15:25:02 +0300

    checking with assert exceptions

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
