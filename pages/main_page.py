95143602136e1a088e8a2c66b335439abf3656c7
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-30 09:12:17 +0300

    implicit transition between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
         assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
